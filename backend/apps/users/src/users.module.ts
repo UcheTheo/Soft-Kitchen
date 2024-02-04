6af0bfbfe301b271bb48d087d9f050640965d044
TheSoftNode <thesoftnode@gmail.com>
2024-02-04 18:36:13 +0100

    Configured the gatway's app.module file
    This is configured to use the Apollo gateway and driver

--- backend/apps/users/src/users.module.ts
+++ backend/apps/users/src/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [],
-  //   controllers: [UsersController],
-  controllers: [],
+  controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule {}
